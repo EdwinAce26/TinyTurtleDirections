02504553bf3b4aaae2b7fe181a3f850599e9870a
Edwin <edwin.j.acevedo.01@gmail.com>
2017-01-11 14:46:55 +0000

    tiny turtle

---
+++ script.js
@@ -0,0 +1,16 @@
+  TinyTurtle.apply(window); 
+  //directions begin
+  
+  forward(40);
+  left(90);
+  forward(40);
+  left(90);
+  forward(40);
+  left(90);
+  forward(40);
+  left(90);
+  forward(40);
+  left(35);
+  forward(40);
+  left(118);
+  forward(40);
